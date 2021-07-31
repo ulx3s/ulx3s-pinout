cba13d55c4af449eb0ccb176f18757d73b395cbf
gojimmypi <13059545+gojimmypi@users.noreply.github.com>
2021-07-31 11:57:09 +0200

    print date/time of last change detected

--- pinout-update.sh
+++ pinout-update.sh
@@ -2,7 +2,7 @@
 
 while  [ 1 -eq 1 ]; do 
   inotifywait -e modify styles.css pinout_ulx3s.py data.py
-  echo "Change!"
+  echo "Change! $(date)"
   rm pinout_ulx3s.svg
   python3 -m pinout.manager --export pinout_ulx3s pinout_ulx3s.svg
 done
