c0603c8877570c5af450f7de8b629a46e29e689e
gojimmypi <13059545+gojimmypi@users.noreply.github.com>
2021-07-31 11:43:31 +0200

    initial png2base64 code

---
+++ png2base64/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace png2base64
+{
+    class Program
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="plainText"></param>
+        /// <returns></returns>
+        public static string Base64Encode(string plainText)
+        {
+            // thanks stackoverflow: https://stackoverflow.com/questions/11743160/how-do-i-encode-and-decode-a-base64-string
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            return System.Convert.ToBase64String(plainTextBytes);
+        }
+
+        static void ProcessFile(string f)
+        {
+            Console.WriteLine("Processing file: {0} ...", f);
+
+            string ConstraintText = File.ReadAllText(f);
+
+            File.WriteAllText(f + ".Base64", Base64Encode(ConstraintText));
+        }
+
+        static void Main(string[] args)
+        {
+            string param = args[0];
+
+            if (param != "")
+            {
+                ProcessFile(param);
+            }
+        }
+    }
+}
