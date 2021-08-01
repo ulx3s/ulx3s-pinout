3c3ac2d302fe010ad39d58e3e552c3e79692a0a2
gojimmypi <13059545+gojimmypi@users.noreply.github.com>
2021-08-01 13:28:27 +0200

    add sample svg2png converter

---
+++ svg2png/Program.cs
@@ -0,0 +1,38 @@
+﻿using Svg;
+using System;
+using System.IO;
+
+namespace svg2png
+{
+    // see https://github.com/svg-net/SVG
+
+    class Program
+    {
+        static void ProcessFile(string fileName)
+        {
+            var outputFile = Path.GetDirectoryName(fileName) + @"\" + Path.GetFileNameWithoutExtension(fileName) + ".png";
+            var thisSVG = SvgDocument.Open(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, fileName));
+            thisSVG.Draw().Save(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, outputFile));
+        }
+
+
+        static void Main(string[] args)
+        {
+            if (args.Length == 0) 
+            {
+                Console.WriteLine("No file specified.");
+                return;
+            }
+
+            string param = args[0];
+
+
+            if (param != "")
+            {
+                ProcessFile(param);
+            }
+
+        }
+    }
+}
+ 
